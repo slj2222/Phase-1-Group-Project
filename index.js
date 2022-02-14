2055f11cd7950d684371dce9b38904421d477635
Spencer Johnson <sljohnson2222@gmail.com>
2022-02-14 12:27:41 -0600

    latest adds

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const submitBtn = docum
 
-    console.log("hello")
+
+
+    fetch("https://statsapi.web.nhl.com/api/v1/schedule/?season=20212022")
+    .then(response => response.json())    
+    .then(data => console.log(data.dates[0].games[0].teams.away.team.name))
+    
+
+
+
 
 
 
